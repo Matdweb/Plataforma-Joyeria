55c77a294c8e1942c1eb521736e11bc629851514
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-07-10 23:09:02 -0600

    preject creation

---
+++ src/main/java/com/joyeria/PlataformaJoyeriaApplication.java
@@ -0,0 +1,13 @@
+package com.joyeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaJoyeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaJoyeriaApplication.class, args);
+	}
+
+}
